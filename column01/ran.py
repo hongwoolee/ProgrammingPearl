1374f3f5a4de11bd78dd7566d4f9a04285fb2c60
hongwoo <hongwoo7@gmail.com>
2014-02-16 00:38:36 +0900

    random generator 수정.

--- column01/ran.py
+++ column01/ran.py
@@ -3,14 +3,7 @@ import random
 maxLength = 10000000
 L = range(0, maxLength)
 
-count = 0
-while count < maxLength:
-    i = random.randint(0, maxLength-count-1)
-    if i < 0:
-        break
+random.shuffle(L)
+for i in L:
     print L[i]
-    del L[i]
-    count = count + 1
-
-
 
